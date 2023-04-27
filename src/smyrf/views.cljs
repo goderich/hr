7780e4427e368f14a9a310dfca232388d647e4ca
Goderich <yuhsien77@gmail.com>
2023-04-27 22:41:22 +0800

    Add styling

--- src/smyrf/views.cljs
+++ src/smyrf/views.cljs
@@ -14,15 +14,15 @@
 (defn view-payments [ms]
   (let [details? (rf/subscribe [::subs/details?])]
     (when @details?
-      (into [:div]
+      (into [:div.details]
             (for [m ms]
-              [:div
-               (str (-> m :month date->str) "："
-                    "勞退為" (:pension m) "元，"
-                    "健保為" (:health m) "元"
-                    (:health-comment m) "，"
-                    (:days m) "天校方勞保支出為"
-                    (:labor m) "元" (:labor-comment m) "。")])))))
+              [:p
+               [:div (str (-> m :month date->str) "：")]
+               [:div (str "勞退為" (:pension m) "元")]
+               [:div (str "健保為" (:health m) "元"
+                          (:health-comment m))]
+               [:div (str (:days m) "天校方勞保支出為"
+                          (:labor m) "元" (:labor-comment m))]])))))
 
 (defn view-node [node]
   (let [event-fn
@@ -30,9 +30,10 @@
           (events/value-change {:type type :value value :node node}))]
     [:div
      "月薪："
-     [:input {:type "text"
-              :placeholder "請輸入月薪"
-              :on-change #(event-fn :text %)}]
+     [:input.input
+      {:type "text"
+       :placeholder "請輸入月薪"
+       :on-change #(event-fn :text %)}]
      [:div
       "開始："
       [:input {:type "date"
@@ -47,22 +48,28 @@
     `[:div ~@views]))
 
 (defn view-total [m]
-  [:div
-   (str
-    "總額試算：勞退為" (:pension m) "元，"
-    "健保為" (:health m) "元，"
-    "勞保為" (:labor m) "元。")])
+  [:p.total
+   [:div (str "總額試算：")]
+   [:div (str "勞退為 " (:pension m) " 元")]
+   [:div (str "健保為 " (:health m) " 元")]
+   [:div (str "勞保為 " (:labor m) " 元")]
+   ])
 
 (defn main-panel []
   (let [details? (rf/subscribe [::subs/details?])
         nodes (rf/subscribe [::subs/nodes])]
     [:div
      [:h1 "Simple 仁侍!"]
-     [:p
-      [:input {:type "checkbox"
-               :checked @details?
-               :on-change #(rf/dispatch [::events/check-details])}]
-      "顯示細算資料？"]
      [view-nodes @nodes]
-     [:button {:on-click #(rf/dispatch [::events/add])} "+"]
-     [view-total (calc/total @nodes)]]))
+     [view-total (calc/total @nodes)]
+     [:div.meta-container
+      [:div.container
+       [:button.add {:on-click #(rf/dispatch [::events/add])} "+"]]
+      [:div.container
+       [:input {:type "checkbox"
+                :checked @details?
+                :on-change #(rf/dispatch [::events/check-details])}]
+       "顯示細算資料？"]]
+     [:p]
+     [:p.footer
+      "Made by 郭育賢"]]))
